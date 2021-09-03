2b2bfe5a9470140f70a2bb7e32ef74283b6075b9
231saurabh <saurabhrajigare9696@gmail.com>
2021-09-03 09:45:18 -0700

    First Commit

---
+++ Recursion-problem3.js
@@ -0,0 +1,16 @@
+//3- Implement a function that takes two numbers, testVariable1 and testVariable2 and returns their greatest common divisor.
+
+function gcd_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+      return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+  
+  console.log(gcd_two_numbers(6, 9));
